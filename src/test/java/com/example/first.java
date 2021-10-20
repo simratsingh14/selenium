4d0c32effac2300a523432ea613e3304e40034b5
simrat_singh <simratsingh14@gmail.com>
2021-10-20 15:56:26 +0530

    experimented

--- src/test/java/com/example/first.java
+++ src/test/java/com/example/first.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -10,7 +12,10 @@ public class first {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://google.com");
+        driver.manage().window().maximize();
+        driver.get("https://google.com/");
+        driver.findElement(By.name("q")).sendKeys("Selenium");
+        // driver.findElement(By.name("btnK")).click();
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
