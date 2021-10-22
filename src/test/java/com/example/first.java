df6d4b56fb1cb7ea671e99415324ed1b57311792
simrat_singh <simratsingh14@gmail.com>
2021-10-22 15:23:26 +0530

    practiced different selectors type

--- src/test/java/com/example/first.java
+++ src/test/java/com/example/first.java
@@ -1,9 +1,11 @@
 package com.example;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.idealized.Javascript;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -12,12 +14,16 @@ public class first {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        driver.get("https://ebay.com");
         driver.manage().window().maximize();
-        driver.get("https://google.com/");
-        driver.findElement(By.name("q")).sendKeys("Selenium");
+        // driver.findElement(By.name("q")).sendKeys("Selenium");
         // driver.findElement(By.name("btnK")).click();
+        JavascriptExecutor js  = (JavascriptExecutor) driver;
+        driver.findElement(By.cssSelector("#gh-ac")).sendKeys("Pillows");
+        driver.findElement(By.cssSelector("#gh-btn")).click();
+        js.executeScript("window.scrollBy(0,300)");
         try {
-            Thread.sleep(3000);
+            Thread.sleep(3000); 
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
